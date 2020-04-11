1113660c8b6e747fe95a88a5864fa991851fb9db
Carly <carlyne.lapierre@gmail.com>
2020-04-11 18:18:37 +0200

    adding hover effect on img

--- src/app/services/galleryItemsData.service.ts
+++ src/app/services/galleryItemsData.service.ts
@@ -1,59 +1,66 @@
 export class GalleryItemsData {
   
-  getRandomStyleSizing() {
-    const sizes = ["w--sm", "w--md", "w--lg"];
-    return sizes[Math.floor(Math.random() * sizes.length)];
+  getRandomStyleSizing(category) {
+    const imgSizes = ["w--sm", "w--md"],
+          sizes = ["w--sm", "w--md", "w--lg"];
+
+    if (category === 'illustration') {
+      return imgSizes[Math.floor(Math.random() * imgSizes.length)];
+    } else {
+      return sizes[Math.floor(Math.random() * sizes.length)];
+    }
+    
   }
   
   itemsThumbnails = [
     {
       id: 1,
       name: 'Illustration1',
-      srcImg: '../assets/img/illus.png',
+      srcImg: '../assets/img/etiquette.jpg',
       category: 'illustration',
-      size: this.getRandomStyleSizing()
+      size: this.getRandomStyleSizing('illustration')
     },
     {
       id: 2,
       name: 'my project',
       srcImg: '../assets/img/projet.png',
       category: 'project',
-      size: this.getRandomStyleSizing()
+      size: this.getRandomStyleSizing('project')
     },
     {
       id: 3,
       name: 'my quote',
       category: 'quote',
-      content: 'lorem ipsum dolore situa mentoris',
-      size: this.getRandomStyleSizing()
+      content: 'Il faut toute la vie pour apprendre à vivre',
+      size: this.getRandomStyleSizing('quote')
     },
     {
       id: 4,
       name: 'Illustration1',
-      srcImg: '../assets/img/illus.png',
+      srcImg: '../assets/img/01.jpg',
       category: 'illustration',
-      size: this.getRandomStyleSizing()
+      size: this.getRandomStyleSizing('illustration')
     },
     {
       id: 5,
       name: 'Illustration1',
-      srcImg: '../assets/img/illus.png',
+      srcImg: '../assets/img/02.jpg',
       category: 'illustration',
-      size: this.getRandomStyleSizing()
+      size: this.getRandomStyleSizing('illustration')
     },
     {
       id: 6,
       name: 'my project',
       srcImg: '../assets/img/projet.png',
       category: 'project',
-      size: this.getRandomStyleSizing()
+      size: this.getRandomStyleSizing('project')
     },
     {
       id: 7,
       name: 'my quote',
       category: 'quote',
       content: 'lorem ipsum dolore situa mentoris',
-      size: this.getRandomStyleSizing()
+      size: this.getRandomStyleSizing('quote')
     }
   ]
 
